2d180c3ecef320373e11c6b20de7474f8d756b20
kommusravani <104352484+kommusravani@users.noreply.github.com>
2022-04-25 14:32:34 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,16 @@
+"""Date:21 april Thursday 
+Names:Sravani
+File desc:data types
+"""
+print(type(2))             #int
+print(type(2.5))           #float
+print(type('sravani'))     #str
+print(type("sravani"))     #str
+print(type([2]))           #list
+print(type((2,3)))         #tuple
+print(type({5}))           #set
+print(type({2:'python'}))  #dict
+print(type(True))          #bool
+print(type(False))         #bool
+print(type(4+2j))          #complex
+print(type(b'hello'))      #bytes
---
+++ printstatement.py
@@ -0,0 +1,13 @@
+"""Date:19 april 2022 Tuesdday  
+Name:Sravani
+File desc:print statement
+"""
+a="python basics"
+print(a)  #printing string
+b=5
+print(b)  #printing int
+c=7.8
+print(c)  #printing float
+print("pthon programming language")
+
+#if anything we want to display we should write with in a print statement 
